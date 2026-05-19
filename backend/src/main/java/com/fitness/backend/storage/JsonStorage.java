2be97b34740556cbd6d52de9ab00a852ff0f42b0
davidandrus1 <davidandrua@gmail.com>
2026-05-19 19:27:30 +0300

    Structura

--- backend/src/main/java/com/fitness/backend/storage/JsonStorage.java
+++ backend/src/main/java/com/fitness/backend/storage/JsonStorage.java
@@ -12,14 +12,24 @@ import java.util.List;
 @Component
 public class JsonStorage {
 
-    private static final String DATA_PATH = System.getProperty("user.dir") 
-    + File.separator + "data" 
-    + File.separator;
+    private static String getDataPath() {
+    File f = new File("data");
+    if (f.exists()) return "data" + File.separator;
+    
+    File f2 = new File("backend/data");
+    if (f2.exists()) return "backend" + File.separator + "data" + File.separator;
+    
+    File f3 = new File("../data");
+    if (f3.exists()) return ".." + File.separator + "data" + File.separator;
+    
+    return "data" + File.separator;
+    }
     
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static <T> List<T> read(String fileName, Class<T> clazz) {
-        File file = new File(DATA_PATH + fileName + ".json");
+        File file = new File(getDataPath() + fileName + ".json");
+        System.out.println("Caut fisierul la: " + file.getAbsolutePath());
         if (!file.exists()) return new ArrayList<>();
         try {
             return mapper.readValue(file,
@@ -30,7 +40,7 @@ public class JsonStorage {
     }
 
     public static <T> void write(String fileName, List<T> data) {
-        File file = new File(DATA_PATH + fileName + ".json");
+        File file = new File(getDataPath() + fileName + ".json");
         try {
             mapper.writerWithDefaultPrettyPrinter().writeValue(file, data);
         } catch (IOException e) {
